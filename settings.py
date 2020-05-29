56be1f78bd577d912cd90d030bd4db174071ee97
Srijan <srijan.dhungana6@gmail.com>
2020-05-29 20:37:22 +0300

    change

--- settings.py
+++ settings.py
@@ -5,13 +5,13 @@ class Settings():
         self.screen_w = 1000  #screen width
         self.screen_h = 600   #screen height
         self.bg_colour = (60,60,60)
-        self.speed = 3
+        self.speed = 1
         self.b_speed = 10
         self.bullet_w = 40
         self.bullet_h = 10
         self.bullet_colour = 255, 0, 0
         self.max_bullets = 2
-        self.drone_speed = 0.3
+        self.drone_speed = 0.05
         self.drone_s_speed = 1
         self.speed_up = 0.05
         self.lives = 5
